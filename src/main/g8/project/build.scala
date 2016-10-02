bf9c5392aad2b98733d3c8460417b79ad25a94d6
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2016-10-02 21:34:45 +0100

    Compiling Jetty in.

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -37,7 +37,7 @@ object $name;format="Camel"$Build extends Build {
         "org.scalatra" %% "scalatra-swagger" % ScalatraVersion,
         "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.1.5" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container;compile",
         "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
